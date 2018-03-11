94710e3193576006ff4d15f5435b484a5f730797
myfour <oz_myx@126.com>
2018-03-12 01:19:28 +0800

    条件判断

---
+++ python_conditionJudge.py
@@ -0,0 +1,19 @@
+# if
+x = 3
+if x < 10:
+    print('进入if的语句中')
+# if-else
+if x < 10:
+    print('x<10的判断中')
+else:
+    print('x>10了')
+# if-else-if
+if x > 10:
+    print('x>10')
+elif x > 6:
+    print('x>6')
+else:
+    print('x<6')
+# 条件判断中，只要是非零的数值，非空字符串，非空list等，就为True
+if x:
+    print(True)
